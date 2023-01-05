b0c2073e4132c93c7c3e061f9f4b2972632dff7d
Abhinav Saxena <abhinav150601@gmail.com>
2023-01-05 23:12:52 +0530

    added UI

--- popnow/lib/main.dart
+++ popnow/lib/main.dart
@@ -1,72 +1,29 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const popnow());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class popnow extends StatelessWidget {
+  const popnow({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+      return MaterialApp(
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-
+        backgroundColor: Colors.black,
+        brightness: Brightness.dark,
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-    
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      initialRoute: '/',
+      routes:
+      {
+        '/': (context) => const homepage(),
+        // '/second': (context) => const SecondRoute(),
+      },
     );
   }
 }
